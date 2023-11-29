35feb0ab85834e21d01b34ef5863ba2a43a442ad
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2023-11-29 08:52:20 +0100

    clase vehiculo

--- Scripts/vehiculo.js
+++ Scripts/vehiculo.js
@@ -1,7 +1,35 @@
-class Vehiculo{
-    #marca;
-    #modelo;
-    #color;
-    #fabricacion;
-    #cilindrada;
+class Vehiculo {
+  //Privados
+  #marca;
+  #modelo;
+  #color;
+  #fabricacion;
+  #cilindrada;
+
+  //    Constructor con parámetros
+  constructor(marca, modelo, color, fabricacion, cilindrada) {
+    this.#marca = marca;
+    this.#modelo = modelo;
+    this.#color = color;
+    this.#fabricacion = fabricacion;
+    this.#cilindrada = cilindrada;
+  }
+  //    constructor por defecto o sin parámetros
+  constructor(){}
+
+  mostrarDatos(){
+    const datos = `La marca es -> ${this.marca}
+    <br>
+    El modelo es -> ${this.modelo}
+    <br>
+    Su color es -> ${this.color}
+    <br>
+    Se fabricó el año -> ${this.fabricacion}
+    <br>
+    Cilindrada -> ${this.cilindrada}
+    `;
+
+    //  Llamar a la función mostrarResultado definida en el main.js
+    mostrarResultado(`<p>${datos}</p>`)
+  }
 }
