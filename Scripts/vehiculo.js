32eb9cb823a8bb8027ea3fdd0304f3ec0fbf5d17
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2023-11-29 09:06:35 +0100

    vehiculo

--- Scripts/vehiculo.js
+++ Scripts/vehiculo.js
@@ -29,7 +29,62 @@ class Vehiculo {
     Cilindrada -> ${this.cilindrada}
     `;
 
-    //  Llamar a la función mostrarResultado definida en el main.js
+    //  Llamar a la función mostrarResultado definida en el main.js, con node.js esta línea desaparece
     mostrarResultado(`<p>${datos}</p>`)
+  }
+
+  acelerar(velocidad){
+    const mensaje = `<br>Se ha acelerado hasta -> ${velocidad}km/h`;
+    mostrarResultado(`<p>${datos}</p>`)
+  }
+
+  arrancar(){
+    const mensaje = `<br>El coche ${this.#marca}, ${this.#modelo}, de color ${this.#color} ha arrancado`;
+    mostrarResultado(`<p>${datos}</p>`)
+  }
+
+  frenar(){
+    const mensaje = `<br>El coche ${this.#marca}, ${this.#modelo}, de color ${this.#color} ha parado`;
+    mostrarResultado(`<p>${datos}</p>`)
+  }
+
+  get marca(){
+    return this.#marca;
+  }
+
+  set marca(value){
+    this.#marca = value;
+  }
+
+  get modelo(){
+    return this.#modelo;
+  }
+
+  set modelo(value){
+    this.#modelo = value;
+  }
+
+  get color(){
+    return this.#color;
+  }
+
+  set color(value){
+    this.#color = value;
+  }
+
+  get fabricacion(){
+    return this.#fabricacion;
+  }
+
+  set fabricacion(value){
+    this.#fabricacion = value;
+  }
+
+  get cilindrada(){
+    return this.#cilindrada;
+  }
+
+  set cilindrada(value){
+    this.#cilindrada = value;
   }
 }
