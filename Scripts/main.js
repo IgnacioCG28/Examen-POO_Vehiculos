0ae0134bb5192142635e75113fdfcdfd3ee19bad
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2023-12-01 14:10:10 +0100

    localStorage

--- Scripts/main.js
+++ Scripts/main.js
@@ -11,6 +11,25 @@ export function mostrarResultado(mensaje) {
   resultadoContenedor.innerHTML += `<p>${mensaje}</p>`;
 }
 
+// Variable de tipo Object de un objeto vehículo
+
+const vehiculoObject = {
+  marca: "BMW",
+  modelo: "CLK",
+  color: "rojo",
+  fabricacion: 2011,
+  cilindrada: 2000,
+};
+
+//Almacenamos datos = Guardar el JSON en el LocalStorage
+localStorage.setItem("vehiculo", JSON.stringify(vehiculoObject));
+
+//Mostrar datos- Mostrar el Object en consola
+console.log(JSON.parse(localStorage.getItem("vehiculo")));
+
+// Imprimimos JSON por consola
+console.log(localStorage.getItem("vehiculo"));
+
 // Muestra de un objeto vehículo
 
 const vehiculo1 = new Vehiculo("Toyota", "Yaris", "Rojo", 2011, 1200);
@@ -27,9 +46,47 @@ furgoneta1.arrancar();
 furgoneta1.acelerar(60);
 furgoneta1.frenar();
 
-const todoterreno1 = new Todoterreno("Range Rover", "Defender", "Negro", 2023, 2030, true);
+// Guardamos en el localStorage la marca de una furgoneta
+
+localStorage.setItem("Marca Furgoneta", furgoneta1.marca);
+
+const todoterreno1 = new Todoterreno(
+  "Range Rover",
+  "Defender",
+  "Negro",
+  2023,
+  2030,
+  true
+);
 mostrarResultado(`<h2>--Vehículo 3 Todoterreno--</h2>`);
 todoterreno1.mostrarDatos();
 todoterreno1.arrancar();
 todoterreno1.acelerar(70);
 todoterreno1.frenar();
+
+//Generar variable tipo object con POO
+
+const todoterrenoObject = {
+  marca: todoterreno1.marca,
+  modelo: todoterreno1.modelo,
+  color: todoterreno1.color,
+  fabricacion: todoterreno1.fabricacion,
+  cilindrada: todoterreno1.cilindrada,
+  traccion: todoterreno1.traccion,
+};
+
+for(let key in todoterrenoObject){
+  // hasOwnProperty lo utilizamos para almacenar el nombre de la propiedad de cada objeto
+  if(todoterrenoObject.hasOwnProperty(key)){
+    localStorage.setItem(key,todoterrenoObject[key]);
+  }
+}
+
+// Eliminar elemento localStorage
+
+localStorage.removeItem('color');
+localStorage.removeItem('marca')
+
+// Eliminar todo en localStorage
+
+localStorage.clear();
